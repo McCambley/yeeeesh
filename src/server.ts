36d40900b84ca039d02efa41c3f54ddb0a9cb84e
McCambley <jakemccamb16@gmail.com>
2022-09-23 00:17:25 -0400

    🗑 Deprecate express server

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,42 @@
-import express, { Request, Response } from "express";
-import { getPlaylistData, getAccessToken, getArtists } from "./utils/spotifyClient";
-import twitterClient from "./utils/twitterClient";
+// import express, { Request, Response } from "express";
+// import { getPlaylistData, getAccessToken, getArtists } from "./utils/spotifyClient";
+// import twitterClient from "./utils/twitterClient";
 
-const app = express();
-const port = process.env.PORT || 3000;
+// const app = express();
+// const port = process.env.PORT || 3000;
 
-app.get("/", async (req: Request, res: Response) => {
-  try {
-    const accessToken = await getAccessToken();
-    let data = await getPlaylistData(accessToken);
-    if (data instanceof Array) {
-      for (const song of data) {
-        const tweetContent = `${getArtists(song.track.artists)} — ${song.track.name}\n${song.track.external_urls.spotify}`;
+// app.get("/", async (req: Request, res: Response) => {
+//   try {
+//     const accessToken = await getAccessToken();
+//     let data = await getPlaylistData(accessToken);
+//     if (data instanceof Array) {
+//       for (const song of data) {
+//         const tweetContent = `${getArtists(song.track.artists)} — ${song.track.name}\n${song.track.external_urls.spotify}`;
 
-        console.log(tweetContent);
-        const tweet = await twitterClient.v2.tweet(tweetContent);
-        console.log(tweet);
-        await new Promise((resolve) => {
-          setTimeout(resolve, 4800);
-        });
-      }
-    }
-    res.send(data);
-  } catch (error) {
-    res.send({ error });
-  }
-});
+//         console.log(tweetContent);
+//         const tweet = await twitterClient.v2.tweet(tweetContent);
+//         console.log(tweet);
+//         await new Promise((resolve) => {
+//           setTimeout(resolve, 4800);
+//         });
+//       }
+//     }
+//     res.send(data);
+//   } catch (error) {
+//     res.send({ error });
+//   }
+// });
 
-app.get("/tweet", async (req: Request, res: Response) => {
-  try {
-    const data = await twitterClient.v2.tweet("Yeeeesh...");
-    res.send(data);
-  } catch (error: any) {
-    res.send(error);
-  }
-});
+// app.get("/tweet", async (req: Request, res: Response) => {
+//   try {
+//     const data = await twitterClient.v2.tweet("Yeeeesh...");
+//     res.send(data);
+//   } catch (error: any) {
+//     res.send(error);
+//   }
+// });
 
-app.listen(port, () => {
-  // cron.schedule("* * * * *", () => console.log("Soooon!"));
-  console.log("Welcome back to ✨ Express ✨ my friend");
-});
+// app.listen(port, () => {
+//   // cron.schedule("* * * * *", () => console.log("Soooon!"));
+//   console.log("Welcome back to ✨ Express ✨ my friend");
+// });
