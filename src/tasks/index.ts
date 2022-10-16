eedffda7f0b42f90116410ce91d8db645892dca9
McCambley <jakemccamb16@gmail.com>
2022-10-16 00:05:43 -0400

    ♻️ Create read only logger

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -2,10 +2,12 @@ import nodeCron from "node-cron";
 import { Crons } from "../types/types";
 import { checkForUpdates } from "./checkForUpdates";
 import { logStatus } from "./logStatus";
+import { checkFile } from "./checkFile";
 
 const crons: Crons = [
   { cron: "0 * * * *", task: checkForUpdates },
   { cron: "* * * * *", task: logStatus },
+  // { cron: "* * * * *", task: checkFile },
 ];
 
 const setCrons = (): void => {
